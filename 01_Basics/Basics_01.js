581dae5073d3313bbf6d930cfcb566151c847589
rohaibhussain <rohaibhussain469@email.com>
2026-03-09 03:23:59 +0500

    Some Basics has done in JS

---
+++ 01_Basics/Basics_01.js
@@ -0,0 +1,17 @@
+// 
+
+// Blocked , function , global scope in "Var"
+// var a = 20;
+// {
+//   var a = 21;
+//   console.log("inside :",a)
+// }
+// console.log("outside :",a)
+
+// Blocked , function , global scope in "let"
+let b = 30;
+{
+  let b = 35;
+  console.log("inside B", b)
+}
+console.log("outside B",b)
