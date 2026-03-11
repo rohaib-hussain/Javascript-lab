ca56ff628e1cc7d4ce473282396a41769acf82a3
rohaibhussain <rohaibhussain469@email.com>
2026-03-11 06:41:31 +0500

    Add solutions for different loop problems

---
+++ 01_Basics/loopQuestionAnswers.js
@@ -0,0 +1,109 @@
+//Following are the Different Types of loops Quesitons and solutions :
+
+
+  // Q1 for(let i=1 ; i<11 ; i++){
+  //   console.log(i);
+  // }
+
+
+
+  // Q2
+  // let count=10;
+  //   while(count>0){
+  //     console.log(count);
+  //     count-- 
+  //   }
+
+
+
+  // Q3
+  // for(let count=1 ; count<21 ; count++){
+  //   if(count%2===0){
+  //   console.log(count);
+  //   }
+  // }
+
+
+
+  //Q4 (Odd Num using Loop)
+  // let count=1;
+  //  while(count<16){
+  //   if(count%2===1){ 
+  //     console.log(count); 
+  //   }
+  //   count++
+  //  }
+
+
+
+  //Q5 (`5` Table)
+  // for(let count=1 ; count<11 ; count++){
+  //   console.log(`5 x 1 = ${5 * count}`); 
+  // }
+
+
+  //Q6
+  //  let sum = 0;
+  //  for(let i=1 ; i<101 ; i++){
+  //   sum = sum + i
+  // }
+  // console.log(sum) => if we put console in for block it will print console again and again that's why it's outside if block.
+  
+
+  //Q7 (Loop which is divisible by 3)
+  // for(let count=1 ; count<51 ; count++){
+  //   if(count%3===0){
+  //     console.log(count); 
+  //   }
+  // }
+
+
+  //Q8 (Loop with user Input)
+  // let num = prompt(`give a number`)
+  // for (i=1 ; i<=num ; i++){
+  //   if(i%2===0){
+  //     console.log(`${i} is even`);
+  //   } else{
+  //     console.log(`${i} is odd`);  
+  //   }
+  // }
+
+
+  //Q9 (using `&&` Operator)
+  // for(let i=1; i<101 ; i++){
+  //   if(i%3===0 && i%5===0){
+  //     console.log(i)
+  //   }
+  // }
+
+
+  //Q10 (Break or switch loop)
+  // for(count=1 ; count<101 ; count++){
+  //  console.log(count)
+  //   if(count%7===0){
+  //     break;
+  //     }
+  // }
+
+
+  //Q11 (Continue Loop)
+  //  for(count=1 ; count<31 ; count++){
+  //   if(count%7===0) continue;
+  //   console.log(count);
+  //  }
+
+
+
+
+  //Q12 (Printing Only first 5 Odd Numbers)
+  // let Num=0;
+  // for(count=1 ; count<101 ; count++){
+  //   if(count%2===1){
+  //     Num++;
+  //     console.log(count);
+  //   }
+  //   if(Num===5) break;
+  // }
+
+
+  //Q13
