e4ec25f9ddc56125b57d3b34dd1d61ca3da5490f
rohaibhussain <rohaibhussain469@email.com>
2026-03-13 03:22:01 +0500

    function in js done !

---
+++ 01_Basics/functions.js
@@ -0,0 +1,71 @@
+              // Functions //
+
+// when we want to put our code and print it for next time or in future , we put it into function to avoid instant printing .
+
+
+
+  // syntax of function //
+   // function(){
+   // };
+   //();
+
+
+  //function statement: (type)
+  // function Intro() {
+  //   console.log(`This is called function statement`)
+  // };
+  //  Intro();     <== called to function
+
+
+
+
+ // function expression: (type)
+
+  // let myName=function(){
+  //   console.log(`write your name here`);
+  // }
+  // myName();
+
+
+
+
+ //Fat arrow function: (type)
+
+//  let fatArrow=()=>{
+//   console.log(`this is called fat arrow function`);
+//  }
+//  fatArrow();
+
+
+
+ 
+// function myCode(name1 , name2){
+//   console.log(name1 + name2);  
+// }
+// myCode(99,1);
+
+
+
+// //another function
+// function nameNum (n1 , n2){
+//   console.log(n1 + n2);
+// }
+//  nameNum(`Rohaib`,1);
+
+
+
+// Default parameter (when we give a value to parameter variable its' called default para . examples are following )
+
+// function defPara(v1=3, v2=4){      //<== given value to var as a default .
+//   console.log(v1 ,v2); 
+// }
+// defPara();
+
+
+// another e.g
+// function concat(v1=`Rohaib` , v2=`Hussain`){
+//   console.log(v1 + v2);
+// }
+// concat();
+
+ 
