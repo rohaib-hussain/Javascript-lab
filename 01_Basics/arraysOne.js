edf94cbdf49b2720503eb45b671ebcfc315a515f
rohaibhussain <rohaibhussain469@email.com>
2026-03-14 01:41:31 +0500

    arrays part one

---
+++ 01_Basics/arraysOne.js
@@ -0,0 +1,67 @@
+// when we put several values in one variable it's called array. we can have more than one values in array .
+
+//  let arr =[1 ,2 ,4 ,7];
+//  arr[0];
+
+
+
+  //  arrays methods:
+
+
+  // push method (mean to add another value at last) 
+  // let arr = [1,2,3,4]
+  //  arr.push(5);  //(5 added)
+
+
+
+
+  // pop method ( mean to remove a value)
+  // let arr = [1,2,3,4]
+  //  arr.pop(); //(removed 4)
+
+
+
+  //shift methods (removed value from start)
+  // let arr = [1,2,3,4]
+  //  arr.shift();  //(1 removed)
+
+
+  //unshift method (add value at start)
+// let arr = [1,2,3,4]
+//    arr.unshift(5); //(added 5 at start)
+
+
+
+//splice method (remove value from the middle)
+// let arr = [1,2,3,4,5]
+//    arr.splice(2,1); // it removed 3.
+
+
+
+//slice method(it give us a new value but in a new variable , doesn't change present one)
+// let arr = [1,2,3,4,5]
+//   let  newarr = arr.slice(2,1); 
+
+
+
+// reverse method (reverse the values , actual values)
+// let arr = [1,2,3,4,5,6]
+//    arr.reverse();
+
+
+
+
+// sort method (make values in order descending or acsending but it creates a function)
+
+// let arr = [2,1,5,3,7,44]
+//    let sr = arr.sort(function(a,b){
+//     return a - b; //(ascending order)
+//    });
+
+ //another e.g for descending order.
+//  let arr = [2,1,5,3,7,44]
+//    let sr = arr.sort(function(a,b){
+//     return b-a; //(ascending order)
+//    });
+
+
