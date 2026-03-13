dc1dcdb16e0ed856816c9c268017c6cc62d011b3
rohaibhussain <rohaibhussain469@email.com>
2026-03-14 02:29:40 +0500

    arrays part two with some extra concepts

---
+++ 01_Basics/arraysTwo.js
@@ -0,0 +1,36 @@
+// forEach method :
+// (for each method calls a function for every and each element in array)
+
+// let arr = [10,20,30,40,50]
+// arr.forEach(function(val){
+//  console.log(val+10);      // so for each add 10 in each value/element in array
+// })
+
+
+
+
+// map method (use map only when you wanna creat a new array on the basis of previous array's data)
+
+// let arr = [2,1,5,3,20,10]
+//    let newarr = arr.map(function(val){
+//      return 5;   //(add 5 in all elements of "newarr" array)
+//    })
+
+
+
+
+//Destructuring (it's using when we wanna get specific values from an array )
+
+// let arr = [10,25,15,30,50]
+// let [a, b, ,d,e]=arr;
+
+
+
+//spread operator (...) //it basically a method to copy an array without changing it's actual value as usual .
+
+// let arr = [1,2,3,4,5,6,7]
+// let newArr = [...arr]
+//so this spread operator method won't change actual value of arr , it will change values only in newArr.
+
+
+ 
