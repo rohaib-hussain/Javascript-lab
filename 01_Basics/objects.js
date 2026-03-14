d9eda0a9033b10dba86ced2f891f4c1fd7f9b04b
rohaibhussain <rohaibhussain469@email.com>
2026-03-14 22:21:59 +0500

    All about Objects in js

---
+++ 01_Basics/objects.js
@@ -0,0 +1,76 @@
+// object are variable that can store both values and functions .
+// let obj ={
+//   Name : `Rohaib Hussain`,
+//   age : 20,
+//   adress : `Pakistan , KPK`,
+// }
+// obj.Name
+
+
+
+//Q1
+// let obj ={
+//   name : `Rohaib`,
+//   age : 20,
+//   isEnroled : true,
+// }
+// obj.isEnroled;
+
+
+
+
+//Q2
+// const user={
+//   "first-name":"Rohaib",
+// };
+// user["first-name"]
+
+
+
+
+//Q3
+// const locations = {
+//   city: "Peshawar",
+//   coordinates: {
+//     lat : 23.2,
+//     lng : 72.3
+//   },
+// };
+//  locations.coordinates.lat
+
+
+
+
+//Q4
+// Destructure the city and lat from the location oject
+// const locations = {
+//   city: "Peshawar",
+//   coordinates: {
+//     lat : 23.2,
+//     lng : 72.3
+//   },
+// };
+// let {city}=locations;
+// let {lat}=locations.Coordinates,
+
+
+
+
+
+//Q5 (using for in )
+// const user={
+//   name :"Rohaib",
+//   adress : "KPK" 
+// };
+
+// for(let key in user){
+//   console.log(key);
+  
+// }
+
+//Q6 (.entries)
+//   const user={
+//   name :"Rohaib",
+//   adress : "KPK" 
+// };
+// Object.entries(user)
